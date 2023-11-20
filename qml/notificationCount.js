daae2148709668393a5273b5f7093dd9525bba66
Vlad G <b100dian@gmail.com>
2023-11-20 11:24:14 +0200

    Use current session for notifications

--- qml/notificationCount.js
+++ qml/notificationCount.js
@@ -1,9 +1,25 @@
 
-function refreshSession() {
-    var session0 = JSON.parse(localStorage.getItem("hydrogen_sessions_v1"))[0].id;
-    var result = indexedDB.open("hydrogen_session_" + session0);
-    result.onerror = function(error) { console.error("open", error); }
+function getCurrentSession() {
+    var matches = window.location.hash.match(/session\/(\d+)/);
+    if (matches && matches.length > 1) {
+        return matches[1];
+    } else {
+        var sessions = JSON.parse(localStorage.getItem("hydrogen_sessions_v1"));
+        var recent = sessions.sort((x, y) => x.lastUsed - y.lastUsed).pop();
+        if (recent) {
+            return recent.id;
+        }
+    }
+}
 
+function refreshSession() {
+    var currentSession = getCurrentSession();
+    if (!currentSession) {
+        return;
+    }
+
+    var result = indexedDB.open("hydrogen_session_" + currentSession);
+    result.onerror = function(error) { console.error("open", error); }
 
     result.onsuccess = function(event) {
         var db = event.target.result;
@@ -16,12 +32,11 @@ function refreshSession() {
           console.error(event);
         };
         request.onsuccess = function(event) {
-          console.log(`Result is`,request.result);
           var notificationCount = request.result.reduce((sum, item) => sum + item.notificationCount, 0);
-          // Most recent 5 discussions with unread:
+          // Most recent 10 discussions with unread:
           let top5 = request.result.filter(item => !!item.notificationCount)
                 .sort((x,y) => x.lastMessageTimestamp - y.lastMessageTimestamp)
-                .slice(-5).map(item => item.name || item.heroes[0])
+                .slice(-10).map(item => item.name || item.heroes[0])
                 .reverse();
 
           var customEvent = new CustomEvent("framescript:notificationCount",
