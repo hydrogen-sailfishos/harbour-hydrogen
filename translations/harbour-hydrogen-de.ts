1dcf402fd5a5cdea6c9810c52afd6c6babca3605
nephros <nemo@pgxperiiia10>
2023-11-21 10:33:49 +0100

    [L10N] Update translations
    Translate German

--- translations/harbour-hydrogen-de.ts
+++ translations/harbour-hydrogen-de.ts
@@ -2,24 +2,76 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>AppSettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <comment>page title</comment>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <source>Application</source>
+        <translation>Applikation</translation>
+    </message>
+    <message>
+        <source>Web View</source>
+        <translation>Web View</translation>
+    </message>
+    <message>
+        <source>Theme Mode</source>
+        <translation>Thema-Modus</translation>
+    </message>
+    <message>
+        <source>Whether we will follow the Ambience style, or used fixed Light or Dark mode.</source>
+        <translation>Ob wir dem Ambienten-Stil folgen, oder immer den Hellen oder Dunklen Modus verwenden.</translation>
+    </message>
+    <message>
+        <source>Light Mode</source>
+        <translation>Heller Modus</translation>
+    </message>
+    <message>
+        <source>Dark Mode</source>
+        <translation>Dunkler Modus</translation>
+    </message>
+    <message>
+        <source>Follow Ambience</source>
+        <translation>Ambiente folgen</translation>
+    </message>
+    <message>
+        <source>Scale user interface</source>
+        <translation>Benuteroberfläche skalieren</translation>
+    </message>
+    <message>
+        <source>Page Zoom factor</source>
+        <translation>Vergrößerungsfaktor</translation>
+    </message>
+</context>
+<context>
     <name>HydrogenCover</name>
     <message>
         <source>Hydrogen</source>
-        <translation type="unfinished"></translation>
+        <translation>Hydrogen</translation>
     </message>
 </context>
 <context>
     <name>HydrogenWebView</name>
     <message>
         <source>Messages: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Nachrichten</translation>
     </message>
 </context>
 <context>
     <name>HydrogenWebViewPage</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished">Einstellungen</translation>
+        <translation type="obsolete">Einstellungen</translation>
+    </message>
+    <message>
+        <source>App Settings</source>
+        <translation>App-Einstellungen</translation>
+    </message>
+    <message>
+        <source>Hydrogen Settings</source>
+        <translation>Hydrogen-Einstellungen</translation>
     </message>
 </context>
 </TS>
--- translations/harbour-hydrogen-ro.ts
+++ translations/harbour-hydrogen-ro.ts
@@ -2,6 +2,50 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>AppSettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <comment>page title</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Web View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Theme Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Whether we will follow the Ambience style, or used fixed Light or Dark mode.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Light Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dark Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Follow Ambience</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Scale user interface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Page Zoom factor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>HydrogenCover</name>
     <message>
         <source>Hydrogen</source>
@@ -18,7 +62,11 @@
 <context>
     <name>HydrogenWebViewPage</name>
     <message>
-        <source>Settings</source>
+        <source>App Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hydrogen Settings</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
