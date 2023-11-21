588fabc7f6d55b711ff957d5bd8d1e9b4832f74f
nephros <nemo@pgxperiiia10>
2023-11-21 17:12:53 +0100

    Fix Pyton header

--- qml/server.py
+++ qml/server.py
@@ -1,7 +1,7 @@
-// Copyright © 2021-2023 thigg
-// Copyright © 2023 The SailfishOS Hackathon Budapest Team
-//
-// SPDX-License-Identifier: Apache-2.0
+# Copyright © 2021-2023 thigg
+# Copyright © 2023 The SailfishOS Hackathon Budapest Team
+#
+# SPDX-License-Identifier: Apache-2.0
 
 import pyotherside
 import threading
