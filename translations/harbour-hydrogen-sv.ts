ede6578a12340489ce1b4a9e2650d5a91b712bd2
Åke Engelbrektson <eson57@users.noreply.github.com>
2023-12-25 09:42:28 +0100

    Create harbour-hydrogen-sv.ts
    Add Swedish translation

---
+++ translations/harbour-hydrogen-sv.ts
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AppSettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <comment>page title</comment>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Application</source>
+        <translation type="vanished">Applikation</translation>
+    </message>
+    <message>
+        <source>Web View</source>
+        <translation>Webbvy</translation>
+    </message>
+    <message>
+        <source>Theme Mode</source>
+        <translation>Temaläge</translation>
+    </message>
+    <message>
+        <source>Whether we will follow the Ambience style, or used fixed Light or Dark mode.</source>
+        <translation>Om vi skall följa atmosfärstilen eller använda fast ljust eller mörkt läge.</translation>
+    </message>
+    <message>
+        <source>Light Mode</source>
+        <translation>Ljust läge</translation>
+    </message>
+    <message>
+        <source>Dark Mode</source>
+        <translation>Mörkt läge</translation>
+    </message>
+    <message>
+        <source>Follow Ambience</source>
+        <translation>Följ atmosfär</translation>
+    </message>
+    <message>
+        <source>Scale user interface</source>
+        <translation>Skala användargränssnitt</translation>
+    </message>
+    <message>
+        <source>Page Zoom factor</source>
+        <translation>Sidzoomfaktor</translation>
+    </message>
+    <message>
+        <source>Show Notifications</source>
+        <translation>Visa avisering</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Avisering</translation>
+    </message>
+    <message>
+        <source>Sticky Notifications</source>
+        <translation>Fäst avisering</translation>
+    </message>
+    <message>
+        <source>If enabled, the app will update a single notification (as opposed to sending a new one each time).</source>
+        <translation>Vid aktivering kommer appen att uppdatera en singelavisering (i motsats till att skicka en ny varje gång).</translation>
+    </message>
+    <message>
+        <source>Restart the App to apply changes to any of the values below.</source>
+        <translation>Stata om appen för att tillämpa ändringar i något av nedanstående värden.</translation>
+    </message>
+    <message>
+        <source>Browser Memory</source>
+        <translation>Bläddringsminne</translation>
+    </message>
+    <message>
+        <source>automatic</source>
+        <translation>Automatiskt</translation>
+    </message>
+    <message>
+        <source>disabled</source>
+        <translation>Inaktiverat</translation>
+    </message>
+    <message>
+        <source>MB</source>
+        <translation>MB</translation>
+    </message>
+    <message>
+        <source>Memory Cache</source>
+        <translation>Minnescache</translation>
+    </message>
+</context>
+<context>
+    <name>HydrogenCover</name>
+    <message>
+        <source>Hydrogen</source>
+        <translation>Hydrogen</translation>
+    </message>
+</context>
+<context>
+    <name>HydrogenWebView</name>
+    <message>
+        <source>Messages: %1</source>
+        <translation>Meddelanden: %1</translation>
+    </message>
+</context>
+<context>
+    <name>HydrogenWebViewPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="obsolete">Inställningar</translation>
+    </message>
+    <message>
+        <source>App Settings</source>
+        <translation>Appinställningar</translation>
+    </message>
+    <message>
+        <source>Hydrogen Settings</source>
+        <translation>Hydrogeninställningar</translation>
+    </message>
+</context>
+</TS>
