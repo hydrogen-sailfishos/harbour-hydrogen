52856fa3c97a8697e102633e11d2b17bdf0a1ade
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2024-11-28 15:16:51 +0100

    Generalize object store access

--- qml/notificationCount.js
+++ qml/notificationCount.js
@@ -5,6 +5,31 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+/*
+ * Access an object store.
+ *
+ * Arguments:
+ *   db: opened indexedDB
+ *   name: str
+ *   onError: function(request: request, event: str)
+ *   onSuccess: function(request: request, event: str)
+ */
+function _accessObjectStore(db, name, onError, onSuccess) {
+    var transaction = db.transaction(name, "readonly", "relaxed");
+    var storeNames = Array.from(transaction.objectStoreNames);
+
+    if (storeNames.indexOf(name) === -1) {
+        onError("object store '" + name + "' not found");
+    }
+
+    var objectStore = transaction.objectStore(name);
+
+    var request = objectStore.getAll();
+
+    request.onerror = function(event) { onError(request, event); }
+    request.onsuccess = function(event) { onSuccess(request, event); }
+}
+
 function getCurrentSession() {
     var matches = window.location.hash.match(/session\/(\d+)/);
     if (matches && matches.length > 1) {
@@ -24,36 +49,28 @@ function refreshSession() {
     result.onsuccess = function(event) {
         var db = event.target.result;
 
-        var transaction = db.transaction("roomSummary", "readonly", "relaxed");
-        var storeNames = Array.from(transaction.objectStoreNames);
-        if (storeNames.indexOf("roomSummary") === -1) return;
+        _accessObjectStore(db, "roomSummary", function(request, event){
+            console.error(event);
+        }, function(request, event) {
+            var notificationCount = request.result.reduce((sum, item) => sum + item.notificationCount, 0);
 
-        var objectStore = transaction.objectStore("roomSummary");
+            // Most recent bunch of discussions with unread
+            let mostRecentCount = 20;
+            let coverPreview = request.result.filter(item => !!item.notificationCount)
+                    .sort((x,y) => x.lastMessageTimestamp - y.lastMessageTimestamp)
+                    .slice(-mostRecentCount).map(item => ({
+                        name: item.name || item.heroes[0],
+                        count: item.notificationCount,
+                    })).reverse();
 
-        var request = objectStore.getAll();
-        request.onerror = function(event) {
-          console.error(event);
-        };
-        request.onsuccess = function(event) {
-          var notificationCount = request.result.reduce((sum, item) => sum + item.notificationCount, 0);
-
-          // Most recent bunch of discussions with unread
-          let mostRecentCount = 20;
-          let coverPreview = request.result.filter(item => !!item.notificationCount)
-                .sort((x,y) => x.lastMessageTimestamp - y.lastMessageTimestamp)
-                .slice(-mostRecentCount).map(item => ({
-                    name: item.name || item.heroes[0],
-                    count: item.notificationCount,
-                })).reverse();
-
-          var customEvent = new CustomEvent("framescript:notificationCount", {
-              detail: {
-                  count: notificationCount,
-                  coverPreview: coverPreview,
-              }
-          });
-          document.dispatchEvent(customEvent);
-        };
+            var customEvent = new CustomEvent("framescript:notificationCount", {
+                detail: {
+                    count: notificationCount,
+                    coverPreview: coverPreview,
+                }
+            });
+            document.dispatchEvent(customEvent);
+        });
     }
 }
 
